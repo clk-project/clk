7f05eb41b071871c9229bdb8294449748a36368e
Samuel Loury <konubinixweb@gmail.com>
2018-04-06 10:18:15 +0200

    Expose simple method to handle 90% of use cases

--- click_project/setup.py
+++ click_project/setup.py
@@ -5,7 +5,7 @@ from __future__ import print_function, absolute_import
 
 from click_project.flow import setup as setup_flow
 from click_project.triggers import setup as setup_triggers
-from click_project.overloads import CoreCommandResolver, MainCommand
+from click_project.overloads import CoreCommandResolver, MainCommand, entry_point
 from click_project.scriptcommands import ScriptCommandResolver
 from click_project.externalcommands import ExternalCommandResolver
 from click_project.alias import DottedAliasResolver, AliasCommandResolver, AliasToGroupResolver
@@ -15,11 +15,12 @@ from click_project.completion import init as completion_init
 from click_project.config import setup_config_class, Config
 from click_project.log import get_logger, basic_config
 from click_project import lib
+from click_project.core import main  # NOQA: F401
 
 LOGGER = get_logger(__name__)
 
 
-def classic_setup(main_module, config_cls=Config, extra_command_packages=[]):
+def classic_setup(main_module=None, config_cls=Config, extra_command_packages=[]):
     lib.main_module = main_module
     completion_init()
     setup_config_class(config_cls)
@@ -50,4 +51,10 @@ def classic_setup(main_module, config_cls=Config, extra_command_packages=[]):
     def decorator(command):
         config_cls.main_command = command
         return command
+    return decorator
+
+
+def basic_entry_point(main_module, extra_command_packages=[]):
+    def decorator(f):
+        return classic_setup(main_module, extra_command_packages=extra_command_packages)(entry_point()(f))
     return decorator
