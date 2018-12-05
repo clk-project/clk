0c13f9e07cb80beed7be5ec3f874c64274741402
Samuel Loury <konubinixweb@gmail.com>
2018-12-05 08:10:30 +0100

    Try to fallback on the default value in case the parameters is required but missing

--- click_project/overloads.py
+++ click_project/overloads.py
@@ -15,6 +15,7 @@ import click
 import click_didyoumean
 import six
 from click.utils import make_default_short_help
+from click.exceptions import MissingParameter
 
 from click_project.click_helpers import click_get_current_context_safe
 from click_project.core import get_ctx, rebuild_path, settings_stores,\
@@ -783,8 +784,12 @@ class ParameterMixin(click.Parameter):
         return value
 
     def full_process_value(self, ctx, value):
-        value = super(ParameterMixin, self).full_process_value(ctx, value)
-        value = self.__process_value(ctx, value)
+        try:
+            value = super(ParameterMixin, self).full_process_value(ctx, value)
+        except MissingParameter as e:
+            value = self.__process_value(ctx, value)
+            if value is None:
+                raise e
         return value
 
     def get_path(self, ctx):
