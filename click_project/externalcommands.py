cce6095ceab7f8eb4f20b5bddd37393ed544f2f9
Samuel Loury <konubinixweb@gmail.com>
2019-05-07 08:05:11 +0200

    Parse the help of external commands to guess the parameters

--- click_project/externalcommands.py
+++ click_project/externalcommands.py
@@ -6,6 +6,9 @@ from __future__ import print_function, absolute_import
 import os
 import subprocess
 import re
+import importlib
+
+import click
 
 from click_project.commandresolver import CommandResolver
 from click_project.config import config
@@ -50,21 +53,59 @@ class ExternalCommandResolver(CommandResolver):
         command_name = prefix + name
         paths = external_cmds_paths()
         command_path = which(command_name, os.pathsep.join(paths))
+        options = []
+        arguments = []
+        flags = []
+        remaining_args = "Remaining arguments"
         try:
             process = subprocess.Popen([command_path, "--help"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = process.communicate()
             if process.returncode == 0:
                 out = out.decode("utf-8")
-                cmdhelp_lines = out.splitlines()
+                cmdhelp_lines = out.splitlines() + ['']
                 try:
                     index_desc = cmdhelp_lines.index('') + 1
                 except ValueError:
-                    index_desc = None
-                if index_desc is None:
-                    cmdhelp = out
-                else:
-                    cmdhelp = cmdhelp_lines[index_desc]
+                    index_desc = 0
+                try:
+                    metadata_desc = cmdhelp_lines.index('--')
+                    remaining_args = False
+                except ValueError:
+                    metadata_desc = -1
+                cmdhelp = "\n".join(cmdhelp_lines[index_desc:metadata_desc])
                 cmdhelp = cmdhelp.strip()
+                metadata_out = out[metadata_desc:]
+                for l in metadata_out.splitlines():
+                    if l.startswith("O:"):
+                        m = re.match("^O:(?P<name>[^:]+):(?P<type>[^:]+):(?P<help>[^:]+)$", l)
+                        if m is None:
+                            raise click.UsageError(
+                                "Expected format in {} is O:name:type:help,"
+                                " got {}".format(path, l))
+                        options.append(
+                            m.groupdict()
+                        )
+                    if l.startswith("F:"):
+                        m = re.match("^F:(?P<name>[^:]+):(?P<help>[^:]+)$", l)
+                        if m is None:
+                            raise click.UsageError(
+                                "Expected format in {} is F:name:help,"
+                                " got {}".format(path, l))
+                        flags.append(
+                            m.groupdict()
+                        )
+                    if l.startswith("A:"):
+                        m = re.match("^A:(?P<name>[^:]+):(?P<type>[^:]+):(?P<help>[^:]+)(:(?P<nargs>[^:]+))?$", l)
+                        if m is None:
+                            raise click.UsageError(
+                                "Expected format in {} is A:name:type:help[:nargs],"
+                                " got {}".format(path, l))
+                        arguments.append(
+                            m.groupdict()
+                        )
+                    m = re.match("^N:(?P<help>[^:]+)$", l)
+                    if m is not None:
+                        remaining_args = m.group("help")
                 cmdflowdepends = re.search('flowdepends: (.+)', out)
                 if cmdflowdepends:
                     cmdflowdepends = cmdflowdepends.group(1).split(', ')
@@ -80,14 +121,53 @@ class ExternalCommandResolver(CommandResolver):
             on_command_loading_error()
             raise
 
-        from click_project.decorators import command, argument
+        from click_project.decorators import command, argument, option, flag
 
-        @command(name=name, help=cmdhelp, ignore_unknown_options=True,
-                 add_help_option=False, short_help=cmdhelp.splitlines()[0],
-                 handle_dry_run=True,
-                 flowdepends=cmdflowdepends)
-        @argument('args', nargs=-1)
-        def external_command(args):
+        def external_command(**kwargs):
             from click_project.lib import call
-            call([command_path] + list(args))
+            args = (
+                [command_path]
+                + config.command_line_settings["parameters"][path]
+            )
+            call(
+                args
+            )
+
+        for o in options:
+            if "type" in o:
+                t = o["type"].split(".")
+                m = importlib.import_module(".".join(t[:-1]))
+                t = getattr(m, t[-1])
+            external_command = option(
+                o["name"],
+                help=o["help"],
+                type=t or str,
+            )(external_command)
+        for a in arguments:
+            if "type" in a:
+                t = a["type"].split(".")
+                m = importlib.import_module(".".join(t[:-1]))
+                t = getattr(m, t[-1])
+            external_command = argument(
+                a["name"],
+                help=a["help"],
+                type=t or str,
+                nargs=int(a.get("nargs", "1")),
+            )(external_command)
+        for f in flags:
+            external_command = flag(
+                f["name"],
+                help=f["help"],
+            )(external_command)
+        if remaining_args:
+            external_command = argument('args', nargs=-1, help=remaining_args)(external_command)
+
+        external_command = command(
+            name=name,
+            help=cmdhelp,
+            short_help=cmdhelp.splitlines()[0] if cmdhelp else "",
+            handle_dry_run=True,
+            flowdepends=cmdflowdepends)(
+                external_command
+            )
         return external_command
