a048c746993aaa2a7088f3e485fb017a07352ad2
Samuel Loury <konubinixweb@gmail.com>
2020-08-31 22:33:09 +0200

    Add a command to resolve commands

--- click_project/commands/commands.py
+++ click_project/commands/commands.py
@@ -6,17 +6,24 @@ from __future__ import print_function, absolute_import
 import click
 
 from click_project.config import config
-from click_project.decorators import command, pass_context
+from click_project.decorators import (
+    group,
+    argument,
+)
 from click_project.log import get_logger
-from click_project.overloads import Group
+from click_project.overloads import Group, CommandType, get_command
 
 LOGGER = get_logger(__name__)
 
 
-@command(handle_dry_run=True)
-@pass_context
-def commands(ctx):
+@group(handle_dry_run=True, default_command="display")
+def commands():
     """Display all the available commands"""
+
+
+@commands.command()
+def display():
+    ctx = click.get_current_context()
     display_subcommands(ctx, config.main_command)
 
 
@@ -41,3 +48,13 @@ def cmd_format(name, cmd_help, indent):
     end = len(indent) + len(name)
     spacer = ' ' * max(20 - end, 1)
     return indent + name + spacer + cmd_help
+
+
+@commands.command()
+@argument("path", type=CommandType(), help="The command to resolve")
+def resolve(path):
+    """Resolve a command to help understanding where a command comes from"""
+    cmd, resolver = get_command(path, True)
+    click.echo(
+        f"The command {path} is resolved by the resolver {resolver.name}"
+    )
