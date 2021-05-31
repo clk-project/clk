bbb0b5be3cd79f322f1d91a46ef41b06da09b7d3
Vincent Hobeïka <vincent.hobeika@gmail.com>
2021-05-31 16:07:41 +0200

    Remove unused variables

--- click_project/_version.py
+++ click_project/_version.py
@@ -109,11 +109,11 @@ def versions_from_parentdir(parentdir_prefix, root, verbose):
 
     Source tarballs conventionally unpack into a directory that includes both
     the project name and a version string. We will also support searching up
-    two directory levels for an appropriately named parent directory
+    two directory levels for an appropriately named parent directory.
     """
     rootdirs = []
 
-    for i in range(3):
+    for _ in range(3):
         dirname = os.path.basename(root)
         if dirname.startswith(parentdir_prefix):
             return {"version": dirname[len(parentdir_prefix):],
@@ -495,7 +495,7 @@ def get_versions():
         # versionfile_source is the relative path from the top of the source
         # tree (where the .git directory might live) to this file. Invert
         # this to find the root from __file__.
-        for i in cfg.versionfile_source.split('/'):
+        for _ in cfg.versionfile_source.split('/'):
             root = os.path.dirname(root)
     except NameError:
         return {"version": "0+unknown", "full-revisionid": None,
