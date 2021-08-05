11db87acaab755f0f60e14a3215bc56ccd0a59ae
Samuel Loury <konubinixweb@gmail.com>
2021-08-05 11:43:16 +0200

    Add a test to find out wheter we can install an extension

---
+++ tests/test_simple_extension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from shlex import split
+from subprocess import check_output
+
+
+def test_simple_extension():
+    check_call(split("clk extension install hello"), encoding='utf8')
+    check_call(split("clk hello"), encoding="utf-8")
