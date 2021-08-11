96ea34813ac7b9686a9689d892501709dec45e3e
Samuel Loury <konubinixweb@gmail.com>
2021-08-11 10:00:14 +0200

    Add the display command that is not default anymore

--- tests/test_command.py
+++ tests/test_command.py
@@ -6,5 +6,5 @@ from subprocess import check_output
 
 
 def test_command():
-    output = check_output(['clk', 'command'], encoding='utf8')
+    output = check_output(['clk', 'command', "display"], encoding='utf8')
     assert re.search(r'flowdep\s+Manipulate command flow dependencies\.', output)
