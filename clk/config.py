ad079376092a522a15a7a8ded3b8d6ea812b1867
Samuel Loury <konubinixweb@gmail.com>
2021-08-23 16:54:34 +0200

    Find a profile, provided a file that belongs to it

--- clk/config.py
+++ clk/config.py
@@ -311,6 +311,11 @@ class Config(object):
         # extensions to enable
         self.settings, self.settings2 = merge_settings(self.iter_settings(recurse=True))
 
+    def get_profile_that_contains(self, path):
+        for profile in self.all_enabled_profiles:
+            if profile.contains(path):
+                return profile
+
     def load_settings_from_profile(self, profile, recurse, only_this_extension=None):
         if profile is not None and (not only_this_extension or profile.short_name == only_this_extension):
             yield profile.settings
