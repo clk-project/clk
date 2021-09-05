013d71359e09dbb07a2d0e7be0648215c37f6806
Samuel Loury <konubinixweb@gmail.com>
2021-09-05 20:15:49 +0200

    Make the parameter precedence test also deal with extensions

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from lib import out, run
+from lib import cmd, out, run
+
+
+def test_parameter_precedence(project1):
+    run('clk parameter set echo global')
+    run(f'clk -P {project1} parameter set echo local')
+    cmd('extension create ext')
+    cmd('parameter set --global-ext echo ext')
+    assert out(f'clk -P {project1} echo') == 'global ext local'
 
 
 def test_simple_parameter():
@@ -14,10 +22,3 @@ def test_parameter_to_alias():
     run('clk parameter set a foo')
     run('clk parameter set echo bar')
     assert out('clk a') == 'bar foo'
-
-
-def test_local_parameter_take_precedence(project1):
-    run(f'clk -P {project1} parameter set echo foo')
-    run('clk parameter set echo bar')
-    assert out('clk echo') == 'bar'
-    assert out(f'clk -P {project1} echo') == 'bar foo'
