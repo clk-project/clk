383703fe7f668aaa7022aebed4c23e9a9546d97c
Samuel Loury <konubinixweb@gmail.com>
2021-09-24 08:46:04 +0200

    Make clearer the intent of the root_dir fixture by yielding it

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,14 +19,14 @@ project1 = project
 
 
 @pytest.fixture(autouse=True)
-def move_somewhere(request):
+def root_dir(request):
     root = tempfile.mkdtemp(prefix=request.node.name[len('test_'):] + '_')
     prev = os.getcwd()
     os.chdir(root)
     os.environ['CLKCONFIGDIR'] = str(Path(root) / 'clk')
     (Path(root) / '.envrc').write_text(f"export CLKCONFIGDIR={os.environ['CLKCONFIGDIR']}")
     run('direnv allow')
-    yield
+    yield root
     del os.environ['CLKCONFIGDIR']
     os.chdir(prev)
 
