cbd00183804db1eda91e1f1efcdb1b942474dbd0
Samuel Loury <konubinixweb@gmail.com>
2021-09-29 16:53:33 +0200

    Add tests about the use of value in parameters

---
+++ tests/test_parameter_eval.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def test_use_value_as_parameter(lib):
+    # when I create a value
+    lib.cmd('value set a b')
+    # and I use that value as a parameter of another command
+    lib.cmd('parameter set echo noeval:value:a')
+    # then, when running the command, the parameters uses the value
+    assert lib.cmd('echo') == 'b'
