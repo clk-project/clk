6562db1eee2c75a01e98225d481b6f5aa11e8383
Samuel Loury <konubinixweb@gmail.com>
2021-10-07 16:50:35 +0200

    Add a test to ensure we can use some environment variables

---
+++ tests/test_environment.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def test_can_manipulate_environments(lib):
+    lib.cmd('env set a b')
+    assert lib.cmd('exec --shell echo ${a}') == 'b'
+
+    assert lib.cmd('env show --no-legend a') == 'a b'
+
+    lib.cmd('env unset a')
+    assert lib.cmd('exec --shell echo ${a}') == ''
