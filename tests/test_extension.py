497ad577fcf0a39d26765b93b1638da4c65c5c79
Samuel Loury <konubinixweb@gmail.com>
2021-10-11 15:09:02 +0200

    More sensible test name

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -5,6 +5,6 @@ from shlex import split
 from subprocess import check_call
 
 
-def test_simple_extension():
+def test_install_extension():
     check_call(split('clk extension install hello'), encoding='utf8')
     check_call(split('clk hello'), encoding='utf-8')
