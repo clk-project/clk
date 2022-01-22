ba200b52bb8457d0b1df5148cd8ed58f303ec4e8
Samuel Loury <konubinixweb@gmail.com>
2022-01-22 23:13:33 +0100

    Add a test to ensure we won't overwrite existing bash commands

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -1,7 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import re
 from pathlib import Path
+from subprocess import PIPE, CalledProcessError
+
+import pytest
+
+
+def test_cannot_remove_existing_command(lib):
+    lib.cmd('command create bash a --no-open')
+    path = lib.out('clk command which a')
+    Path(path).write_text(Path(path).read_text() + """
+echo a""")
+    lib.cmd('command create bash b --no-open')
+    path = lib.out('clk command which b')
+    Path(path).write_text(Path(path).read_text() + """
+echo b""")
+    lib.cmd('extension create ext')
+    lib.cmd('command create --global-ext bash a --no-open')
+    # creating onto another one
+    with pytest.raises(CalledProcessError) as e:
+        lib.cmd('command create bash a --no-open', stderr=PIPE)
+    assert re.match(".*I won't overwrite [/0-9a-zA-Z_-]+/clk/bin/a unless explicitly.*", e.value.stderr)
+    # copying onto another one
+    with pytest.raises(CalledProcessError) as e:
+        lib.cmd('command copy a global/ext a', stderr=PIPE)
+    assert re.match(".*I won't overwrite [/0-9a-zA-Z_-]+/clk/extensions/ext/bin/a unless explicitly.*", e.value.stderr)
+    # moving onto another one
+    with pytest.raises(CalledProcessError) as e:
+        lib.cmd('command move a global/ext', stderr=PIPE)
+    assert re.match(".*I won't overwrite [/0-9a-zA-Z_-]+/clk/extensions/ext/bin/a unless explicitly.*", e.value.stderr)
+    # renaming onto another one
+    with pytest.raises(CalledProcessError) as e:
+        lib.cmd('command rename b a', stderr=PIPE)
+    assert re.match(".*I won't overwrite [/0-9a-zA-Z_-]+/clk/bin/a unless explicitly.*", e.value.stderr)
 
 
 def test_complete_remove(lib):
