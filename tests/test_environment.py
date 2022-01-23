04bee71c5be1ed49f909451ce9a37bb471fe373a
Samuel Loury <konubinixweb@gmail.com>
2022-01-23 16:11:46 +0100

    Factorize some commonly used command

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+show_a_command = 'exec --shell echo ${a}'
+
 
 def test_can_use_evaluated_environments(lib):
     lib.cmd("env set a nexteval:'echo b'")
-    assert lib.cmd('exec --shell echo ${a}') == 'b'
+    assert lib.cmd(show_a_command) == 'b'
 
 
 def test_can_manipulate_environments(lib):
     lib.cmd('env set a b')
-    assert lib.cmd('exec --shell echo ${a}') == 'b'
+    assert lib.cmd(show_a_command) == 'b'
 
     assert lib.cmd('env show --no-legend a') == 'a b'
 
     lib.cmd('env unset a')
-    assert lib.cmd('exec --shell echo ${a}') == ''
+    assert lib.cmd(show_a_command) == ''
