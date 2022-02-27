3cf7c5d39c476783ba39ecebea40c928e7c7dcdc
Samuel Loury <konubinixweb@gmail.com>
2022-02-27 19:40:40 +0100

    Expose a few very common stuff in clk to ease importing

--- clk/__init__.py
+++ clk/__init__.py
@@ -8,6 +8,12 @@ startup_time = datetime.now()
 
 from clk import log  # NOQA: E402
 from clk import monkeypatch  # NOQA: E402
+from clk.config import config  # NOQA: E402, F401
+from clk.core import run  # NOQA: E402, F401
+from clk.decorators import table_format  # NOQA: E402, F401
+from clk.decorators import argument, command, flag, group, option, table_fields, use_settings  # NOQA: E402, F401
+from clk.lib import TablePrinter, call, copy, makedirs, rm  # NOQA: E402, F401
+from clk.log import get_logger  # NOQA: E402, F401
 from clk.overloads import entry_point  # NOQA: E402, F401
 
 monkeypatch.do()
