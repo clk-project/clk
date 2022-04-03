7c3c93b3fe7c28123d97e2ec35287af09268cf8d
Samuel Loury <konubinixweb@gmail.com>
2022-04-03 21:28:35 +0200

    Add a test that shows the verbosity of the flows

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from subprocess import STDOUT
+
 
 def test_dump_flowdeps(lib):
     lib.cmd('flowdep graph --alias-links --format dot')
@@ -59,6 +61,80 @@ upload
 check
 print
 done'''
+
+
+def test_verbose_flow(pythondir, lib):
+    # given a group of commands that allows playing with 3D printing, with a
+    # flow between them and a final command flow
+    (pythondir / 'threed.py').write_text("""
+from clk.decorators import group
+
+
+@group()
+def threed():
+    "3D printing stuff"
+
+
+@threed.command()
+def slicer():
+    "Run the slicer"
+    print("slicer")
+
+
+@threed.command(flowdepends=["threed.slicer"])
+def feed():
+    "Feed the printer"
+    print("feed")
+
+
+@threed.command(flowdepends=["threed.feed"])
+def calib():
+    "Calibrate the printer"
+    print("calib")
+
+
+@threed.command(flowdepends=["threed.calib"])
+def upload():
+    "Send the gcode to the printer"
+    print("upload")
+
+
+@threed.command(flowdepends=["threed.upload"])
+def _print():
+    "Do the actual printing"
+    print("print")
+
+
+@threed.flow_command(flowdepends=["threed.print"])
+def flow():
+    "Run the whole stufff"
+    print("done")
+""")
+    # when I run the flow in verbose mode
+    # then I can see the succession of commands
+    assert lib.cmd(' --verbose-flow threed flow', stderr=STDOUT).splitlines()[1:] == '''--------------
+Running step 'threed slicer'
+slicer
+End of step 'threed slicer'
+--------------
+Running step 'threed feed'
+feed
+End of step 'threed feed'
+--------------
+Running step 'threed calib'
+calib
+End of step 'threed calib'
+--------------
+Running step 'threed upload'
+upload
+End of step 'threed upload'
+--------------
+Running step 'threed print'
+print
+End of step 'threed print'
+Ended executing the flow dependencies, back to the command 'flow'
+done
+'''.splitlines()
 
 
 def test_overwrite_flow(pythondir, lib):
