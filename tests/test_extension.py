2b3b8bdf93709c47e283d0beb4056eed1604911e
Samuel Loury <konubinixweb@gmail.com>
2022-04-20 22:05:16 +0200

    Add a test to verify we can update extensions

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -10,6 +10,12 @@ def test_install_extension():
     check_call(split('clk hello'))
 
 
+def test_update_extension():
+    check_call(split('clk extension install hello'))
+    check_call(split('clk extension update hello'))
+    check_call(split('clk hello --update-extension'))
+
+
 def test_copy_extension(lib):
     lib.cmd('extension create someext')
     lib.cmd('parameter --global-someext set echo test')
