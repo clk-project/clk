fbaaf6f691feb737000fc5fa9b3b7d4a8c9f03eb
Samuel Loury <konubinixweb@gmail.com>
2022-06-29 09:24:17 +0200

    chore: Let the extra arguments of lookup_default pass through
    In click 8, there will be the call argument.

--- clk/monkeypatch.py
+++ clk/monkeypatch.py
@@ -6,11 +6,11 @@ from click import Context
 old_lookup_default = Context.lookup_default
 
 
-def context_lookup_default(self, name):
+def context_lookup_default(self, name, **kwargs):
     if not hasattr(self, 'clk_default_catch'):
         self.clk_default_catch = set()
     self.clk_default_catch.add(name)
-    return old_lookup_default(self, name)
+    return old_lookup_default(self, name, **kwargs)
 
 
 def do():
