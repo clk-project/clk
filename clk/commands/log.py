b118314de95c98b6d06cb13c31e8c66bb4f8dd6f
Samuel Loury <konubinixweb@gmail.com>
2022-09-16 14:46:59 +0200

    feat(log): Add --notify to ease making some logs in scripts that are easier to catch

--- clk/commands/log.py
+++ clk/commands/log.py
@@ -3,7 +3,8 @@
 
 import click
 
-from clk.decorators import argument, command, option
+from clk.decorators import argument, command, flag, option
+from clk.lib import call
 from clk.log import LOG_LEVELS, get_logger
 
 LOGGER = get_logger(__name__)
@@ -11,7 +12,11 @@ LOGGER = get_logger(__name__)
 
 @command(ignore_unknown_options=True, handle_dry_run=True)
 @option('-l', '--level', default='info', type=click.Choice(LOG_LEVELS.keys()), help='Log level')
+@flag("--notify", help="Also send the log into notify-send")
 @argument(u'message', nargs=-1, help='The message to log')
-def log(level, message):
+def log(level, message, notify):
     u"""Log a message"""
-    LOGGER.log(LOG_LEVELS[level], ' '.join(message))
+    message = ' '.join(message)
+    LOGGER.log(LOG_LEVELS[level], message)
+    if notify:
+        call(["notify-send", message])
