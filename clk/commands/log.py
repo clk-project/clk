4c588e0076c41d6dac87b08de780c49168ed0c7f
Samuel Loury <konubinixweb@gmail.com>
2022-09-17 09:23:22 +0200

    chore: Style

--- clk/commands/log.py
+++ clk/commands/log.py
@@ -12,11 +12,11 @@ LOGGER = get_logger(__name__)
 
 @command(ignore_unknown_options=True, handle_dry_run=True)
 @option('-l', '--level', default='info', type=click.Choice(LOG_LEVELS.keys()), help='Log level')
-@flag("--notify", help="Also send the log into notify-send")
+@flag('--notify', help='Also send the log into notify-send')
 @argument(u'message', nargs=-1, help='The message to log')
 def log(level, message, notify):
-    u"""Log a message"""
+    u'''Log a message'''
     message = ' '.join(message)
     LOGGER.log(LOG_LEVELS[level], message)
     if notify:
-        call(["notify-send", message])
+        call(['notify-send', message])
