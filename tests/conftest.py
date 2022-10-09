1ef3918db98e5a6de2c0356eed4fbae28bc5c3c3
Samuel Loury <konubinixweb@gmail.com>
2022-10-09 22:49:11 +0200

    test: Make testing and moving tests around easier by using relocatable envrc, even in source projects

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def rootdir(request):
     os.chdir(root)
     os.environ['CLKCONFIGDIR'] = str(Path(root) / 'clk-root')
     print(root)
-    (Path(root) / '.envrc').write_text(f"export CLKCONFIGDIR={os.environ['CLKCONFIGDIR']}")
+    (Path(root) / '.envrc').write_text('export CLKCONFIGDIR="$(pwd)/clk-root"')
     Lib.run('direnv allow')
     yield root
     del os.environ['CLKCONFIGDIR']
