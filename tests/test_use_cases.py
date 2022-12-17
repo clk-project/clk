f152845f8057c40c21d81f3a04c8de06e5dbe336
Samuel Loury <konubinixweb@gmail.com>
2022-12-17 12:55:35 +0100

    fix: style

--- tests/test_use_cases.py
+++ tests/test_use_cases.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from pathlib import Path
 from subprocess import check_call
 
 
 def call_script(script):
-    use_cases = Path(__file__).parent / "use_cases"
-    check_call(["bash", "-e", "-u", str(use_cases / script)], cwd=str(use_cases))
+    use_cases = Path(__file__).parent / 'use_cases'
+    check_call(['bash', '-e', '-u', str(use_cases / script)], cwd=str(use_cases))
 
 
 def test_bash_command():
-    call_script("bash_command.sh")
+    call_script('bash_command.sh')
 
 
 def test_bash_command_use_option():
-    call_script("bash_command_use_option.sh")
+    call_script('bash_command_use_option.sh')
