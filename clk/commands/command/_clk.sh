1403b33af63a06b1695afe5a4b8079925763efd5
Samuel Loury <konubinixweb@gmail.com>
2025-03-18 16:57:49 +0100

    feat: Add a few useful helpers

--- clk/commands/command/_clk.sh
+++ clk/commands/command/_clk.sh
@@ -76,6 +76,29 @@ clk_list_to_choice () {
     echo "[$(sed -r 's-(.+)- "\1"-'| paste -s - -d,)]"
 }
 
+clk_urldecode () {
+    encoded="$(cat)"
+    encoded="${encoded//+/ }"
+    printf '%b' "${encoded//%/\\x}"
+}
+
+clk_urlencode() {
+    # urlencode <string>
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LC_COLLATE=$old_lc_collate
+}
+
 clk_abort () {
 	local message="${1:-Aborting}"
 	local code="${2:-1}"
