66ba1af5f68016d0dc190a3b34e4d3787bf74c6c
Samuel Loury <konubinixweb@gmail.com>
2026-02-10 09:47:02 +0100

    refactor(alias): Split _get_command to ease reading the code

--- clk/alias.py
+++ clk/alias.py
@@ -133,40 +133,31 @@ class AliasCommandResolver(CommandResolver):
                 if a.startswith(parent.path + ".") and a[len(parent.path) + 1 :] != 0
             ]
 
-    def _get_command(self, path, parent=None):
-        name = path.split(".")[-1]
-        commands_to_run = config.get_settings("alias")[path]["commands"]
-        cmdhelp = config.get_settings("alias")[path]["documentation"]
-        cmdhelp = cmdhelp or f"Alias for: {format_commands(commands_to_run)}"
-        short_help = cmdhelp.splitlines()[0]
-        if len(cmdhelp) > 55:
-            short_help = cmdhelp[:52] + "..."
+    def _collect_flow_dependencies(self, commands_to_run):
+        """Collect flow dependencies from the commands in the alias."""
         deps = []
-
         for cmd in commands_to_run[:-1]:
-            # get the contexts of the commands to capture the flow. I don't want
-            # to allow side effects because they are only supposed to be run,
-            # not to impact how the alias will behave in completion, parameters
-            # etc.
+            # Get contexts without side effects - these commands are only run,
+            # not meant to impact alias behavior in completion, parameters, etc.
             cmdctx = get_ctx(cmd, resilient_parsing=True, side_effects=False)
-            # I capture the flow only if it has not already been consumed by the
-            # command itself
+            # Capture flow only if not already consumed by the command itself
             if not any(
                 cmdctx.params.get(o) for o in ("flow", "flow_after", "flow_from")
             ):
                 deps += flowdeps[cmdctx.command.path]
+        return deps
 
-        # also get the context of the last command to capture its flow. In that
-        # case, I wan't to allow side effects because I want this command to
-        # impact the behavior of the generated alias. Its parameters, completion
-        # or whatever should transpire in the aliased command. The parsing must
-        # be resilient though because the aliased command might be incomplete
-        # (like it might be missing an argument)
-        c = get_ctx(commands_to_run[-1], side_effects=True, resilient_parsing=True)
-        deps += flowdeps[c.command.path]
-        deps = ordered_unique(deps)
+    def _get_last_command_context(self, commands_to_run):
+        """Get context of the last command with side effects enabled.
 
-        kind = None
+        Side effects are enabled because we want this command to impact
+        the behavior of the generated alias (parameters, completion, etc.).
+        Parsing is resilient because the aliased command might be incomplete.
+        """
+        return get_ctx(commands_to_run[-1], side_effects=True, resilient_parsing=True)
+
+    def _determine_command_kind(self, name, cmdhelp, short_help, c, commands_to_run):
+        """Determine the kind of command (group, command, or main) and create decorator."""
 
         def create_cls(cls):
             return cls(
@@ -178,11 +169,9 @@ class AliasCommandResolver(CommandResolver):
 
         if c is not None:
             if isinstance(c.command, Group):
-                cls = create_cls(group)
-                kind = "group"
+                return create_cls(group), "group", c, commands_to_run
             elif isinstance(c.command, Command):
-                cls = create_cls(command)
-                kind = "command"
+                return create_cls(command), "command", c, commands_to_run
             elif isinstance(c.command, config.main_command.__class__):
                 cls = click.group(
                     cls=config.main_command.__class__,
@@ -190,7 +179,7 @@ class AliasCommandResolver(CommandResolver):
                     help=cmdhelp,
                     short_help=short_help,
                 )
-                kind = config.main_command.path
+                return cls, config.main_command.path, c, commands_to_run
             else:
                 raise NotImplementedError()
         elif commands_to_run[-1][0] == config.main_command.path:
@@ -200,24 +189,29 @@ class AliasCommandResolver(CommandResolver):
                 help=cmdhelp,
                 short_help=short_help,
             )
-            del commands_to_run[-1][0]
+            commands_to_run = commands_to_run[:-1] + [commands_to_run[-1][1:]]
             c = get_ctx(commands_to_run[-1])
-            kind = config.main_command.path
+            return cls, config.main_command.path, c, commands_to_run
         else:
-            cls = create_cls(command)
+            return create_cls(command), None, c, commands_to_run
+
+    def _create_alias_callback(self, commands_to_run):
+        """Create the callback function that executes when the alias is invoked."""
 
         def alias_command(ctx, *args, **kwargs):
             if "config" in kwargs:
                 del kwargs["config"]
             commands = list(commands_to_run)
 
+            # Run all commands except the last one
             for command_ in commands[:-1]:
                 LOGGER.debug(
                     "Running command: {}".format(" ".join(quote(c) for c in command_))
                 )
                 run(command_)
-            arguments = ctx.complete_arguments[:]
-            arguments = clean_flow_arguments(arguments)
+
+            # Build the final command with arguments
+            arguments = clean_flow_arguments(ctx.complete_arguments[:])
             if "--" in commands[-1]:
                 separator_index = commands[-1].index("--")
                 processed_part = commands[-1][:separator_index]
@@ -225,47 +219,112 @@ class AliasCommandResolver(CommandResolver):
                 whole_command = processed_part + arguments + ["--"] + ignored_part
             else:
                 whole_command = commands[-1] + arguments
+
+            # Resolve context chain
             original_command_ctx = get_ctx(whole_command, side_effects=True)
             cur_ctx = original_command_ctx
             ctxs = []
-            # if the resolution of the context brought too many commands, we
-            # must not call the call back of the children of the original_command
+            # Don't call callbacks of children of original_command
             while cur_ctx and ctx.command.original_command != cur_ctx.command:
                 cur_ctx = cur_ctx.parent
-
             while cur_ctx:
                 ctxs.insert(0, cur_ctx)
                 cur_ctx = cur_ctx.parent
+
             LOGGER.develop(
                 "Running command: {}".format(" ".join(quote(c) for c in commands[-1]))
             )
 
-            def run_callback(_ctx):
-                LOGGER.develop(
-                    "Running callback of {} with args {}, params {}".format(
-                        _ctx.command.path,
-                        config.commandline_profile.get_settings("parameters")[
-                            _ctx.command.path
-                        ],
-                        _ctx.params,
-                    )
-                )
-                # this alias already captured the complete flow, hence we should
-                # not trigger any flow behavior of the aliased commands
-                for flow_param in "flow", "flow_from", "flow_after":
-                    if flow_param in _ctx.params:
-                        _ctx.params[flow_param] = None
-                with _ctx:
-                    old_resilient_parsing = _ctx.resilient_parsing
-                    _ctx.resilient_parsing = ctx.resilient_parsing
-                    _ctx.command.callback(**_ctx.params)
-                    _ctx.resilient_parsing = old_resilient_parsing
-
+            # Execute callbacks
             for cur_ctx in ctxs:
-                run_callback(cur_ctx)
+                self._run_aliased_callback(cur_ctx, ctx)
 
-        alias_command = pass_context(alias_command)
-        alias_command = cls(alias_command)
+        return alias_command
+
+    def _run_aliased_callback(self, _ctx, parent_ctx):
+        """Run the callback of an aliased command context."""
+        LOGGER.develop(
+            "Running callback of {} with args {}, params {}".format(
+                _ctx.command.path,
+                config.commandline_profile.get_settings("parameters")[
+                    _ctx.command.path
+                ],
+                _ctx.params,
+            )
+        )
+        # Disable flow behavior since alias already captured the complete flow
+        for flow_param in ("flow", "flow_from", "flow_after"):
+            if flow_param in _ctx.params:
+                _ctx.params[flow_param] = None
+        with _ctx:
+            old_resilient_parsing = _ctx.resilient_parsing
+            _ctx.resilient_parsing = parent_ctx.resilient_parsing
+            _ctx.command.callback(**_ctx.params)
+            _ctx.resilient_parsing = old_resilient_parsing
+
+    def _configure_alias_params(self, alias_command, c, kind):
+        """Configure parameters from the original command on the alias."""
+        if c is None:
+            return
+
+        alias_command.original_command = c.command
+        if kind == "group":
+            if c.command.default_cmd_name is not None:
+                alias_command.set_default_command(c.command.default_cmd_name)
+        elif kind == "command":
+            alias_command.handle_dry_run = c.command.handle_dry_run
+
+        alias_param_names = [p.name for p in alias_command.params]
+
+        def was_given(param):
+            return was_explicitly_provided(c, param.name)
+
+        # Add params from original command that aren't already on alias
+        alias_command.params = [
+            param
+            for param in c.command.params
+            if param.name not in alias_param_names
+            and param.name not in ("flow", "flow_from", "flow_after")
+            and (
+                isinstance(param, click.Option)  # options may be given several times
+                or not was_given(param)  # argument not yet given
+                or param.multiple  # may be given again
+                or param.nargs == -1  # may be given again
+            )
+        ] + alias_command.params
+
+        # Mark already-given required params as no longer required
+        for param in alias_command.params:
+            if param.required and was_given(param):
+                param.required = False
+
+    def _get_command(self, path, parent=None):
+        name = path.split(".")[-1]
+        alias_settings = config.get_settings("alias")[path]
+        commands_to_run = alias_settings["commands"]
+        cmdhelp = alias_settings["documentation"]
+        cmdhelp = cmdhelp or f"Alias for: {format_commands(commands_to_run)}"
+        short_help = cmdhelp.splitlines()[0]
+        if len(cmdhelp) > 55:
+            short_help = cmdhelp[:52] + "..."
+
+        # Collect flow dependencies from all commands
+        deps = self._collect_flow_dependencies(commands_to_run)
+        c = self._get_last_command_context(commands_to_run)
+        deps += flowdeps[c.command.path]
+        deps = ordered_unique(deps)
+
+        # Determine command type and get decorator
+        cls, kind, c, commands_to_run = self._determine_command_kind(
+            name, cmdhelp, short_help, c, commands_to_run
+        )
+
+        # Create and decorate the alias callback
+        alias_callback = self._create_alias_callback(commands_to_run)
+        alias_callback = pass_context(alias_callback)
+        alias_command = cls(alias_callback)
+
+        # Add edit option
         alias_command.params.append(
             AutomaticOption(
                 ["--edit-alias"],
@@ -277,6 +336,8 @@ class AliasCommandResolver(CommandResolver):
                 else None,
             )
         )
+
+        # Set flow dependencies if any
         if deps:
             alias_command.clk_flowdepends = deps
             alias_command.clk_flow = c.params.get("flow")
@@ -284,41 +345,6 @@ class AliasCommandResolver(CommandResolver):
             alias_command.clk_flowafter = c.params.get("flow_after")
 
         alias_command.commands_to_run = commands_to_run
-        if c is not None:
-            alias_command.original_command = c.command
-            if kind == "group":
-                if c.command.default_cmd_name is not None:
-                    alias_command.set_default_command(c.command.default_cmd_name)
-            elif kind == "command":
-                alias_command.handle_dry_run = c.command.handle_dry_run
-            alias_param_names = list(map(lambda c: c.name, alias_command.params))
+        self._configure_alias_params(alias_command, c, kind)
 
-            def was_given(param):
-                return was_explicitly_provided(c, param.name)
-
-            alias_command.params = [
-                param
-                for param in c.command.params
-                if param.name not in alias_param_names
-                and param.name not in ("flow", "flow_from", "flow_after")
-                and (
-                    # options may be given several times
-                    isinstance(param, click.Option)
-                    or (
-                        # it is an argument then!
-                        not was_given(param)
-                        or
-                        # may be given, but may be given again
-                        param.multiple
-                        or
-                        # may be given, but may be given again
-                        param.nargs == -1
-                    )
-                )
-            ] + alias_command.params
-            # any option required that was already given should be no more
-            # required
-            for param in alias_command.params:
-                if param.required and was_given(param):
-                    param.required = False
         return alias_command
