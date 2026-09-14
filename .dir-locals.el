8d1f172b84e4a1a48c887193c922b495000dd9e6
Samuel Loury <konubinixweb@gmail.com>
2026-09-14 17:00:06 +0200

    chore: Guide the agent into using the correct command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,13 +1,14 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((konix/agent-shell-tool-whitelist-project . (("^\\./custom-ids\\.sh" . "")
+((nil . ((konix/agent-shell-tool-blacklist-project . (("^\\./tangle\\.sh" . "./tangle-n-export.sh")
+                                                      ("^./doc/use_cases/generate-index.sh$" . "this is done in a pre-commit hook")
+                                                      ("^Edit tests/use_cases/" . "Never touch that file. It is tangled from ./doc/use_cases/")))
+         (konix/agent-shell-tool-whitelist-project . (("^\\./custom-ids\\.sh" . "")
                                                       ("^\\./tangle-n-export\\.sh" . "")
                                                       ("^clk python -m pytest" . "")))
          (konix/agent-shell-mcp-project-servers . ("konix-emacs-code-review"
-                                                   "konix-emacs-workspace"))
-         (konix/agent-shell-tool-blacklist-project . (("^./doc/use_cases/generate-index.sh$" . "this is done in a pre-commit hook")
-                                                      ("^Edit tests/use_cases/" . "Never touch that file. It is tangled from ./doc/use_cases/")))))
+                                                   "konix-emacs-workspace"))))
  (org-mode . ((ispell-dictionary . "american")
               (org-id-link-to-org-use-id . nil)
               (org-babel-default-header-args:python . ((:preserve-indentation . t))))))
